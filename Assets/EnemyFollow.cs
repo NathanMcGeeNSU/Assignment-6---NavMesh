0f1ef3834c1008e682b78fa27d73437b762b8750
Nathan McGee <nathanmcgee85@yahoo.com>
2021-04-17 23:15:49 -0500

    Lowman is here

---
+++ Assets/EnemyFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(Player.position);
+    }
+
+    void OnTiggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameControlScript.health -= 1;
+        }
+    }
+}
